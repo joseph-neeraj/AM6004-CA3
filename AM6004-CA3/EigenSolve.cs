7c2b486829b233987e53bc7fab2a29ca9a55540e
Neeraj Joseph <joseph.neeraj@gmail.com>
2019-03-27 21:49:12 +0000

    Backup

---
+++ AM6004-CA3/EigenSolve.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM6004_CA3
+{
+    class EigenSolve
+    {
+        static public void Householder(ref double[,] A, int n)
+        {
+            for (int k = 0; k < n - 2; k++)
+            {
+
+            }
+        }
+    }
+}
